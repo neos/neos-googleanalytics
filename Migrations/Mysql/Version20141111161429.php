09a57723f71d32875f8522714136462a88dc7bc1
Karsten Dambekalns <karsten@typo3.org>
2015-02-13 23:48:04 +0100

    [!!!][TASK] Add charset and collation to all MySQL migrations
    This change set adds charset and collation to create table statements
    in the existing migrations. This make sure the tables are set up
    correctly independent of the database default configuration.
    
    Also migrations generated contain this information since a while,
    leading to problems on migration if the database is not using the same
    charset and collation.
    
    This is breaking if you have existing tables that do not use the utf8
    charset and utf8_unicode_ci collation. To solve this you need to convert
    the existing tables. This can be done using the command::
    
      ./flow database:setcharset
    
    Change-Id: Ie83df27cba722ebb4576a4e0dad5615af391d6d1
    Releases: master
    Related: NEOS-800

--- Migrations/Mysql/Version20141111161429.php
+++ Migrations/Mysql/Version20141111161429.php
@@ -16,7 +16,7 @@ class Version20141111161429 extends AbstractMigration {
 	public function up(Schema $schema) {
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
 
-		$this->addSql("CREATE TABLE typo3_neos_googleanalytics_domain_model_siteconfiguration (persistence_object_identifier VARCHAR(40) NOT NULL, site VARCHAR(40) DEFAULT NULL, profileid VARCHAR(255) NOT NULL, INDEX IDX_D675F674694309E4 (site), PRIMARY KEY(persistence_object_identifier)) ENGINE = InnoDB");
+		$this->addSql("CREATE TABLE typo3_neos_googleanalytics_domain_model_siteconfiguration (persistence_object_identifier VARCHAR(40) NOT NULL, site VARCHAR(40) DEFAULT NULL, profileid VARCHAR(255) NOT NULL, INDEX IDX_D675F674694309E4 (site), PRIMARY KEY(persistence_object_identifier)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
 		$this->addSql("ALTER TABLE typo3_neos_googleanalytics_domain_model_siteconfiguration ADD CONSTRAINT FK_D675F674694309E4 FOREIGN KEY (site) REFERENCES typo3_neos_domain_model_site (persistence_object_identifier) ON DELETE CASCADE");
 	}
 
