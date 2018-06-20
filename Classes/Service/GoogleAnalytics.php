0a7e7f7b5b64fef2c85be99a4a6a0c124071f52d
Sacha Schmid <s.schmid@schwabe.ch>
2018-06-20 13:08:18 +0200

    Fix code style issues

--- Classes/Service/GoogleAnalytics.php
+++ Classes/Service/GoogleAnalytics.php
@@ -29,9 +29,9 @@ class GoogleAnalytics extends \Google_Service_Analytics
      */
     public function requireAuthentication()
     {
-				if (empty($this->getClient()->getAccessToken())) {
-						throw new AuthenticationRequiredException('No access token', 1415783205);
-				}
+        if (empty($this->getClient()->getAccessToken())) {
+            throw new AuthenticationRequiredException('No access token', 1415783205);
+        }
 
         return $this;
     }
