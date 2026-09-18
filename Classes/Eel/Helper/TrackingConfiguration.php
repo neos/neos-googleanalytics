69b8b1af0298089c37f2c9a1ce84f11467cdeb24
Denny Lubitz <lubitz@vivomedia.de>
2026-09-18 10:59:25 +0200

    TASK: Neos 9 compatibility and removal of backend stats
    Due to GoogleAnalytics API changes to GA4 the backend stats are not working anymore.

--- Classes/Eel/Helper/TrackingConfiguration.php
+++ Classes/Eel/Helper/TrackingConfiguration.php
@@ -13,9 +13,9 @@ namespace Neos\GoogleAnalytics\Eel\Helper;
  * source code.
  */
 
+use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
+use Neos\ContentRepository\Core\SharedModel\Node\NodeName;
 use Neos\Flow\Annotations as Flow;
-use Neos\ContentRepository\Domain\NodeAggregate\NodeName;
-use Neos\ContentRepository\Domain\Projection\Content\NodeInterface;
 use Neos\Eel\ProtectedContextAwareInterface;
 use Neos\Utility\Arrays;
 
@@ -42,21 +42,21 @@ class TrackingConfiguration implements ProtectedContextAwareInterface
      * If no site node is provided, this will get the default settings.
      * If no path is provided, this will get all settings
      *
-     * @param NodeInterface|null $site the site node for which to get settings
+     * @param Node|null $site the site node for which to get settings
      * @param string|array|null $path the settings path
      * @return mixed
      */
-    public function setting(?NodeInterface $site = null, $path = null)
+    public function setting(?Node $site = null, $path = null)
     {
         $settings = $this->defaultSettings;
 
         $nodeName = null;
-        if ($site instanceof NodeInterface) {
-            $nodeName = $site->getNodeName();
+        if ($site instanceof Node) {
+            $nodeName = $site->name;
         }
 
-        if ($nodeName instanceof NodeName && isset($this->sitesSettings[(string)$nodeName])) {
-            $settings = Arrays::arrayMergeRecursiveOverrule($this->defaultSettings, $this->sitesSettings[(string)$nodeName]);
+        if ($nodeName instanceof NodeName && isset($this->sitesSettings[$nodeName->value])) {
+            $settings = Arrays::arrayMergeRecursiveOverrule($this->defaultSettings, $this->sitesSettings[$nodeName->value]);
         }
 
         if ($path === null) {
